ac6b130e6663ad495df460ceba5439b665827e2b
Lucas Santos <lhs.santoss@gmail.com>
2017-01-17 20:29:52 -0200

    Finished download script
    - Added capability of reading multi line files
    - Added download of manual (not chocolatey packages) apps
    - Added base logic for installing
    - Added cleanup
    - Added verbose output

--- Windows/setup.bat
+++ Windows/setup.bat
@@ -8,9 +8,46 @@ echo Installing Choco
 
 REM Installing Apps
 echo Fetching apps
-set /p apps=<apps.ini
+
+set "apps="
+for /f "usebackq delims=" %%a in ("apps.ini") do set "apps=!apps!%%a "
 
 echo Installing following apps:
 echo %apps%
 
 choco install -y %apps%
+
+REM Downloading apps that cannot be installed with choco
+
+echo Downloading manual apps
+
+if not exist downloads mkdir downloads
+
+FOR /F "delims=" %%i in (apps-manual.ini) do (
+  set "line=%%i"
+  REM start "Downloading !line!" "C:\ProgramData\chocolatey\bin\wget.exe" -P ./downloads !line!
+)
+
+cls
+echo Once all downloads finish, hit any key to install
+pause
+
+REM Installing the downloaded apps
+
+echo Installing apps
+
+for %%i in (downloads/*) do (
+  echo Installing %%i
+  start downloads/%%i
+)
+
+
+echo Once all instalations finish, hit any key to delete the files and end
+pause
+
+REM Cleanup
+
+del /F /Q /s downloads/*
+rmdir downloads
+
+exit
