e50f42f1fd4f4452024fab620eda74ad9f147587
Lucas Santos <lhs.santoss@gmail.com>
2017-01-17 20:45:44 -0200

    Added admin check and dotfiles download

--- Windows/setup.bat
+++ Windows/setup.bat
@@ -1,53 +1,84 @@
+title Settin up your computer
+color 0a
+cls
 @echo off
 setlocal enableextensions enabledelayedexpansion
 
-REM Installing Chocolatey
-echo Installing Choco
+goto check_Permissions
 
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
 
-REM Installing Apps
-echo Fetching apps
-
-set "apps="
-for /f "usebackq delims=" %%a in ("apps.ini") do set "apps=!apps!%%a "
-
-echo Installing following apps:
-echo %apps%
-
-choco install -y %apps%
-
-REM Downloading apps that cannot be installed with choco
-
-echo Downloading manual apps
-
-if not exist downloads mkdir downloads
-
-FOR /F "delims=" %%i in (apps-manual.ini) do (
-  set "line=%%i"
-  REM start "Downloading !line!" "C:\ProgramData\chocolatey\bin\wget.exe" -P ./downloads !line!
-)
-
-cls
-echo Once all downloads finish, hit any key to install
-pause
-
-REM Installing the downloaded apps
-
-echo Installing apps
-
-for %%i in (downloads/*) do (
-  echo Installing %%i
-  start downloads/%%i
-)
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+        goto begin
+    ) else (
+        echo Failure: this command prompt needs elevated rights to execute, please execute it again with admin rights.
+        pause
+        exit
+    )
 
 
-echo Once all instalations finish, hit any key to delete the files and end
-pause
+:begin
+  REM Installing Chocolatey
+  echo Installing Choco
 
-REM Cleanup
+  @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-del /F /Q /s downloads/*
-rmdir downloads
+  REM Installing Apps
+  echo Fetching apps
 
-exit
+  set "apps="
+  for /f "usebackq delims=" %%a in ("apps.ini") do set "apps=!apps!%%a "
+
+  echo Installing following apps:
+  echo %apps%
+
+  choco install -y %apps%
+
+  REM Downloading dotfiles
+  echo Downloading dotfiles
+
+  if not exist dotfiles mkdir dotfiles
+
+  FOR /F "delims=" %%i in (dotfiles.ini) do (
+    set "line=%%i"
+    start "Downloading Dotfile !line!" "C:\ProgramData\chocolatey\bin\wget.exe" -P ./dotfiles !line!
+  )
+
+  REM Downloading apps that cannot be installed with choco
+  echo Downloading manual apps
+
+  if not exist downloads mkdir downloads
+
+  FOR /F "delims=" %%i in (apps-manual.ini) do (
+    set "line=%%i"
+    REM start "Downloading !line!" "C:\ProgramData\chocolatey\bin\wget.exe" -P ./downloads !line!
+  )
+
+  cls
+  echo Once all downloads finish, hit any key to install
+  pause
+
+  REM Installing the downloaded apps
+  echo Installing apps
+
+  for %%i in (downloads/*) do (
+    echo Installing %%i
+    start downloads/%%i
+  )
+
+
+  echo Once all instalations finish, hit any key to delete the files and end
+  pause
+
+  REM Cleanup
+  echo Cleaning the house
+
+  del /F /Q /s downloads/*
+  rmdir downloads
+
+
+  echo Bye!
+  exit
