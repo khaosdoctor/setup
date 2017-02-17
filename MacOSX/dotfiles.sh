551bbed8e595bdc43377de5693661f48417787cb
Lucas Santos <lhs.santoss@gmail.com>
2017-02-17 16:00:56 -0200

    Added prompts for personal configs and moved binary

--- MacOSX/dotfiles.sh
+++ MacOSX/dotfiles.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd ~/Desktop
-git clone https://github.com/khaosdoctor/.dotfiles.git dotfiles
+git clone https://github.com/khaosdoctor/.dotfiles.git ~/Desktop/dotfiles
 
 cd ~/Desktop/dotfiles/Global
 
@@ -14,18 +13,26 @@ cd ~/Desktop/dotfiles/MacOSX
 echo -n "Enter path to your .pem files (globs permitted): "
 read pemfiles
 
+echo -n "Enter your git username: "
+read gituser
+
+echo -n "Enter git email: "
+read gitmail
+
 # Replace the token with the typed text
 sed -i "s/!pemfiles!/$pemfiles/g" .bash_profile
+sed -i "s/!user.name!/$gituser/g" .gitconfig
+sed -i "s/!user.email!/$gitmail/g" .gitconfig
 
 mv .bash_profile ~/
 mv .gemrc ~/
 mv .gitconfig ~/
+mv update-all /usr/local/bin/
 
 open Snazzy.itermcolors
 open Snazzy.terminal
 
-cd ~/Desktop
-rm -rf dotfiles
+rm -rf ~/Desktop/dotfiles
 
 exit 0
 
