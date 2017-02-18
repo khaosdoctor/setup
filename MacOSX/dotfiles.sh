110d04324bc40efc39457e324943073d8993b9ce
Lucas Santos <lhs.santoss@gmail.com>
2017-02-18 12:53:09 -0200

    Reordered dotfiles code

--- MacOSX/dotfiles.sh
+++ MacOSX/dotfiles.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+TITLE_START="\033]0;"
+TITLE_END="\007"
+
+echo -ne "${TITLE_START}Downloading DotFiles${TITLE_END}"
+
 git clone https://github.com/khaosdoctor/.dotfiles.git ~/Desktop/dotfiles
 
-cd ~/Desktop/dotfiles/Global
-
-mv .gitexcludes ~/
-mv .vimrc ~/
-
-cd ~/Desktop/dotfiles/MacOSX
+mv ~/Desktop/dotfiles/Global/.gitexcludes ~/
+mv ~/Desktop/dotfiles/Global/.vimrc ~/
+mv ~/Desktop/dotfiles/Global/update-all /usr/local/bin/
 
 # Ask for path
 echo -n "Enter path to your .pem files (globs permitted): "
@@ -20,14 +22,13 @@ echo -n "Enter git email: "
 read gitmail
 
 # Replace the token with the typed text
-sed -i "s/!pemfiles!/$pemfiles/g" .bash_profile
-sed -i "s/!user.name!/$gituser/g" .gitconfig
-sed -i "s/!user.email!/$gitmail/g" .gitconfig
+sed -i "s/!pemfiles!/$pemfiles/g" ~/Desktop/dotfiles/MacOSX/.bash_profile
+sed -i "s/!user.name!/$gituser/g" ~/Desktop/dotfiles/MacOSX/.gitconfig
+sed -i "s/!user.email!/$gitmail/g" ~/Desktop/dotfiles/MacOSX/.gitconfig
 
 mv .bash_profile ~/
 mv .gemrc ~/
 mv .gitconfig ~/
-mv update-all /usr/local/bin/
 
 open Snazzy.itermcolors
 open Snazzy.terminal
