bf864f1137bc20e816e8ed515bae98f492f46016
Lucas Santos <lhs.santoss@gmail.com>
2017-02-18 12:53:18 -0200

    Added Base scaffold

--- MacOSX/setup.sh
+++ MacOSX/setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-echo "Installing brew"
+TITLE_START="\033]0;"
+TITLE_END="\007"
+
+#Color pallete
+RED='\033[0;031m'
+CYAN='\033[0;036m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e "${CYAN}=> Beginning installation process${NC}"
+echo -e "${YELLOW}--THIS MAY REQUIRE YOUR PASSWORD--${NC}"
+
+echo "${TITLE_START}Installing brew${TITLE_END}"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
